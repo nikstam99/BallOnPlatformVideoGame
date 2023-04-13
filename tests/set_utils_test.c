72cc62623e96bbc23b95e77a7abb6bdc7e0807d3
nikstam99 <nikstam99@gmail.com>
2023-04-13 19:06:19 +0300

    project1

--- tests/set_utils_test.c
+++ tests/set_utils_test.c
@@ -12,22 +12,38 @@ int compare_ints(Pointer a, Pointer b){
 }
 
 void void_test_set_func() {
-    
+    Pointer val;
     Set set = set_create(compare_ints, free);
     for (int i = 0; i < 10; i++) {
-        set_insert(set, create_int(i));
+		if (i == 5) continue;
+		val = create_int(i);
+        set_insert(set, val);
     }
     Pointer value = create_int(5);
+	Pointer SMALLER = set_find_eq_or_smaller(set, value);
+	value = create_int(5);
     Pointer GREATER = set_find_eq_or_greater(set, value);
-    Pointer SMALLER = set_find_eq_or_smaller(set, value);
 
-    TEST_ASSERT(compare_ints(GREATER, value) && compare_ints(value, SMALLER));
+    TEST_ASSERT(*(int*)GREATER == 6);
+	TEST_ASSERT(*(int*)SMALLER == 4);
+
+	set_insert(set, create_int(5));
+	value = create_int(5);
+	SMALLER = set_find_eq_or_smaller(set, value);
+	value = create_int(5);
+    GREATER = set_find_eq_or_greater(set, value);
+
+	TEST_ASSERT(*(int*)GREATER == *(int*)SMALLER);
+
+	for (int i = 0; i < 10; i++) {
+		set_remove(set, set_first(set));
+	}
+	free(value);
+	free(val);
 }
 
-
 TEST_LIST = {
-    {"test_set_find_eq_or_greater", set_find_eq_or_greater},
-    {"test_set_find_eq_or_smaller", set_find_eq_or_smaller},
+    {"test_set_find_eq_or_greater", void_test_set_func},
 
     {NULL, NULL}
 };
