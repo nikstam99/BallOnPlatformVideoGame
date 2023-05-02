358d79ad046b36f8f911be0ae57e50e45a06a1a4
nikstam99 <nikstam99@gmail.com>
2023-05-02 10:16:16 +0300

    project1

--- tests/set_utils_test.c
+++ tests/set_utils_test.c
@@ -16,20 +16,25 @@ int compare_ints(Pointer a, Pointer b){
 // Έλεγχος για την λειτουργηκότητα των συναρτήσεων του set_utils.c
 void void_test_set_func() {
     Pointer val;
+
+	// Δημιουργούμε ένα set και του προσθέτουμε 10 αριθμούς.
     Set set = set_create(compare_ints, free);
     for (int i = 0; i < 10; i++) {
 		if (i == 5) continue;
 		val = create_int(i);
         set_insert(set, val);
     }
+	// Ψάχνουμε το 5 με τις συναρτήσεις στο set.
     Pointer value = create_int(5);
 	Pointer SMALLER = set_find_eq_or_smaller(set, value);
 	value = create_int(5);
     Pointer GREATER = set_find_eq_or_greater(set, value);
 
+	// Αφού το 5 δεν υπάρχει ελέγχουμε ότι επιστρέφει σωστά το επόμενο/προηγούμενο στοιχείο.
     TEST_ASSERT(*(int*)GREATER == 6);
 	TEST_ASSERT(*(int*)SMALLER == 4);
 
+	// Προσθέτουμε το 5 στο set και ελέγχουμε ότι το επιστρέφουν οι συναρτήσεις.
 	set_insert(set, create_int(5));
 	value = create_int(5);
 	SMALLER = set_find_eq_or_smaller(set, value);
